ce987f0a1cd5f1da288c7a2157d38330ea7f674d
Priyankaabasavaraj <priyanka.basavaraj@6dtech.co.in>
2022-10-07 18:37:39 +0530

    initial commit

---
+++ src/main/java/com/springboot/kyc/KycManagementApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.kyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KycManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KycManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/kyc/KycManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.kyc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KycManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
